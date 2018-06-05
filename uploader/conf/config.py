e4e069e6b529a505121d127ac0775cd5c6ac749e
studio nixworks <31311297+nixworks@users.noreply.github.com>
2018-06-05 11:42:11 +0200

    Create config.py

---
+++ uploader/conf/config.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Logs
+logs = {
+
+    # level: CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET
+    'level': 'DEBUG',
+    'path': ''
+}
+
+# Tracker infos
+tracker = {
+    'user_authkey': '',
+    'url_announce': ''
+}
+
+# User
+user = {
+    'working_dir': '',
+    'torrents_dir': '',
+    'watch_dir': '',
+    'upload_dir': ''
+}
+
+# API
+api = {
+    'layer13_url': '',
+    'layer13_apikey': '',
+    'srrdb_url': ''
+}
+
+# Release category
+cat = {
+    'movies': '1',
+    'series': '2',
+    'docs': '3',
+    'tv': '4',
+    'sports': '5',
+    'anime': '6',
+    'apps': '7',
+    'games': '8',
+    'ebook': '9',
+    'cartoon': '10'
+}
+
+# Teams
+team = {
+    'animes': [
+        'MANGACiTY' 'BOOLZ', 'D4KiD', 'BOOLZ', 'NEECHAN', 'SaSHiMi',
+        'SHiNiGAMi', 'DIEBEX', 'KAZETV', 'SLEEPINGFOREST', 'OOKAMI'],
+    'games': [
+        'CPY', 'SKiDROW', 'FLT', 'RELOADED', 'PLAZA', 'CODEX']
+}
+
+# Regex
+regex = {
+    'series': r'\.S[0-9]{1,2}[E|x][0-9]{1,3}\.',
+    'movies': r'\.[0-9]{4}\.',
+    'docs': r'\.DOC(S|)\.',
+    'animes': r'\.VOL[0-9]{1,2}\.'
+}
+
+# Patterns
+patterns = {
+
+    # for files only
+    'includes': ['*.nfo', '*.avi', '*.mkv'],
+
+    # for dirs and files
+    'excludes': ['*.srr', '*.flv', '*.sub', '*.idx', '*.nzb', '*sample*']
+}
